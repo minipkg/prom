0ace95601b00a0d299d94070ac2c71e021f0cf4f
Andrey Kalinin <kalinin.andrey1@mail.ru>
2023-10-08 18:49:35 +0300

    added gauge, db_gauge & worker

---
+++ db_gauge.go
@@ -0,0 +1,44 @@
+package prometheus_utils
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// gauge is a struct that allows to add values
+type dbGauge struct {
+	gaugeVec *prometheus.GaugeVec
+}
+
+var _ Gauge = (*dbGauge)(nil)
+
+func NewDBGauge(namespace, subsystem, service, host, dbName string) *dbGauge {
+	gaugeVec := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "gauge",
+		Help: "What is the value of the parameter.",
+		ConstLabels: prometheus.Labels{
+			"namespace": namespace,
+			"subsystem": subsystem,
+			"service":   service,
+			"host":      host,
+			"db":        dbName,
+		},
+	},
+		[]string{"value_name"},
+	)
+
+	prometheus.MustRegister(gaugeVec)
+
+	return &dbGauge{
+		gaugeVec: gaugeVec,
+	}
+}
+
+// Add function adds a given value to the gauge
+func (g *dbGauge) Add(valueName string, value float64) {
+	g.gaugeVec.WithLabelValues(valueName).Add(value)
+}
+
+// Set function sets a given value to the gauge
+func (g *dbGauge) Set(valueName string, value float64) {
+	g.gaugeVec.WithLabelValues(valueName).Set(value)
+}
---
+++ gauge.go
@@ -0,0 +1,47 @@
+package prometheus_utils
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type Gauge interface {
+	Add(valueName string, value float64)
+	Set(valueName string, value float64)
+}
+
+// gauge is a struct that allows to add values
+type gauge struct {
+	gaugeVec *prometheus.GaugeVec
+}
+
+var _ Gauge = (*gauge)(nil)
+
+func NewGauge(namespace, subsystem, service string) *gauge {
+	gaugeVec := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "gauge",
+		Help: "What is the value of the parameter.",
+		ConstLabels: prometheus.Labels{
+			"namespace": namespace,
+			"subsystem": subsystem,
+			"service":   service,
+		},
+	},
+		[]string{"value_name"},
+	)
+
+	prometheus.MustRegister(gaugeVec)
+
+	return &gauge{
+		gaugeVec: gaugeVec,
+	}
+}
+
+// Add function adds a given value to the gauge
+func (g *gauge) Add(valueName string, value float64) {
+	g.gaugeVec.WithLabelValues(valueName).Add(value)
+}
+
+// Set function sets a given value to the gauge
+func (g *gauge) Set(valueName string, value float64) {
+	g.gaugeVec.WithLabelValues(valueName).Set(value)
+}
